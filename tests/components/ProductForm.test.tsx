2225473567fe5ca4ec514f0ccc6af132012a6bfb
Francis Lagares <francis.lagares@gmail.com>
2024-06-19 21:06:59 +0200

    refactor: parameterize tests

--- tests/components/ProductForm.test.tsx
+++ tests/components/ProductForm.test.tsx
@@ -85,24 +85,40 @@ describe('ProductForm Component', () => {
     expect(nameInput).toHaveFocus();
   });
 
-  it('should display error if name is missing', async () => {
-    const { waitForFormToLoad } = renderForm();
+  it.each([
+    {
+      scenario: 'missing',
+      errorMessage: /required/i,
+    },
+    {
+      scenario: 'longer than 255 characters',
+      name: 'a'.repeat(256),
+      errorMessage: /255/i,
+    },
+  ])(
+    'should display error if name is $scenario',
+    async ({ name, errorMessage }) => {
+      const { waitForFormToLoad } = renderForm();
 
-    const form = await waitForFormToLoad();
-    const user = userEvent.setup();
+      const form = await waitForFormToLoad();
+      const user = userEvent.setup();
 
-    await user.type(form.priceInput, '10');
-    await user.click(form.categoryInput);
+      if (name !== undefined) {
+        await user.type(form.nameInput, name);
+      }
+      await user.type(form.priceInput, '10');
+      await user.click(form.categoryInput);
 
-    const options = screen.getAllByRole('option');
-    await user.click(options[0]!);
+      const options = screen.getAllByRole('option');
+      await user.click(options[0]!);
 
-    await user.click(form.submitButton);
+      await user.click(form.submitButton);
 
-    const error = screen.getByRole('alert');
-    expect(error).toBeInTheDocument();
-    expect(error).toHaveTextContent(/required/i);
-  });
+      const error = screen.getByRole('alert');
+      expect(error).toBeInTheDocument();
+      expect(error).toHaveTextContent(errorMessage);
+    },
+  );
 });
 
 const renderForm = (product?: Product) => {
