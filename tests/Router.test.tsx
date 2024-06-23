4a3e3d80e2747dae1782648514afea742b15bebe
Francis Lagares <francis.lagares@gmail.com>
2024-06-23 14:01:43 +0200

    test: routes with parameters

--- tests/Router.test.tsx
+++ tests/Router.test.tsx
@@ -1,6 +1,7 @@
 import { screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 
+import { db } from './mocks/db';
 import { navigateTo } from './utils/router-navigation';
 
 describe('Router Provider', () => {
@@ -16,5 +17,17 @@ describe('Router Provider', () => {
     expect(
       screen.getByRole('heading', { name: /products/i }),
     ).toBeInTheDocument();
+  });
+
+  it('should render the product details page for /products/:id', async () => {
+    const product = db.product.create();
+
+    navigateTo('/products/' + product.id);
+
+    expect(
+      await screen.findByRole('heading', { name: product.name }),
+    ).toBeInTheDocument();
+
+    db.product.delete({ where: { id: { equals: product.id } } });
   });
 });
