17b927973b3571a78c45dde662f70eed9cfa1909
Francis Lagares <francis.lagares@gmail.com>
2024-06-23 14:45:58 +0200

    test: admin page testing

--- tests/Router.test.tsx
+++ tests/Router.test.tsx
@@ -36,4 +36,10 @@ describe('Router Provider', () => {
 
     expect(screen.getByText(/not found/i)).toBeInTheDocument();
   });
+
+  it('should render the admin home page for /admin', () => {
+    navigateTo('/admin');
+
+    expect(screen.getByRole('heading', { name: /admin/i })).toBeInTheDocument();
+  });
 });
